b9c6a89024a2c894a5ddeaf61edea25b3c4ff1aa
Ori Haviv <orihaviv@gmail.com>
2017-06-08 11:10:51 +0300

    .

--- js/script.js
+++ js/script.js
@@ -20,40 +20,39 @@ $(document).ready(function() {
     })
 });
 
+
 function isValidImage(url) {
     return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
 }
 
-function subReddit(text) {
+function subReddit(text){
     $(".posts").empty();
-    if (!text) {
-            text = "art";
-            reddit.hot(text).limit(500)
-                .fetch(function (res) {
-                 if (res) {
-                    var j = -1;
-                    for (var i = 0; i < res.data.children.length; i++) {
-                        var item = res.data.children[i]
-                        if ((item.data.url) && isValidImage(item.data.url)) {
-                            j++;
-                            var content = "<div class='post col-xs-12 col-md-3'><a target='_blank' href='" + "http://reddit.com" + item.data.permalink + "' class='url'><h5 class='postTitle'>" + item.data.title + "</h5><img src='" + item.data.url + "' /></a></div>";
-                            if (j % 4 == 0) {
-                                $(".posts").append("<div class='row'>");
-                                $(".posts").append("<div class='row top-buffer'>");
-                            }
-                            $(".posts").append(content);
-                            $(".posts").append("</div>");
-                        }
-                        if (j == 11) {
-                            break;
-                        }
-                    }
-                    if (j == -1) {
-                        $(".posts").append("<h4>No images were found for this sub-reddit :" + text + " :(</h4>");
-                    }
-                 }
-            }, function (err) {
-                console.log(err);
-            });
+    if (!text){
+        text = "art";
     }
+    reddit.hot(text).limit(500)
+        .fetch(function(res) {
+            if (res){
+                var j =-1;
+                for (var i=0; i < res.data.children.length; i++){
+                    var item = res.data.children[i]
+                    if ((item.data.url) && isValidImage(item.data.url)){
+                        j++;
+                        var content = "<div class='post col-xs-12 col-md-3'><a target='_blank' href='" + "http://reddit.com" + item.data.permalink + "' class='url'><h5 class='postTitle'>" + item.data.title + "</h5><img src='" + item.data.url + "' /></a></div>";
+                        if (j%4 == 0){
+                            $(".posts").append("<div class='row'>");
+                            $(".posts").append("<div class='row top-buffer'>");
+                        }
+                        $(".posts").append(content);
+                        $(".posts").append("</div>");
+                    }
+                    if (j==11){break;}
+                }
+                if (j==-1){
+                    $(".posts").append("<h4>No images were found for this sub-reddit :" + text + " :(</h4>");
+                }
+            }
+        }, function(err){
+            console.log(err);
+        });
 }
